743f30fe75c1e9083af364f10a5ff05488e66bb2
Adam Mckaig <adam.mckaig@gmail.com>
2020-06-19 17:56:51 -0400

    No need to sudo when flashing

--- flash.sh
+++ flash.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash                                                                      
+set -eu -o pipefail
 
 echo "dfu-programmer atmega32u4 erase"
-sudo dfu-programmer atmega32u4 erase
+dfu-programmer atmega32u4 erase
 echo "dfu-programmer atmega32u4 flash kc60_lufa.hex"
-sudo dfu-programmer atmega32u4 flash kc60_lufa.hex
+dfu-programmer atmega32u4 flash kc60_lufa.hex
 echo "dfu-programmer atmega32u4 reset"
-sudo dfu-programmer atmega32u4 reset
+dfu-programmer atmega32u4 reset
 echo "done!"
